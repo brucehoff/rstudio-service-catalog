95acabf80bb573c9830f8ff46ec74b5fe568a3fa
bhoff <bruce.hoff@sagebase.org>
2024-11-13 14:38:54 -0800

    IT-4004:  Add dependency; install versioned rjson

--- install_versioned_package_or_fail.R
+++ install_versioned_package_or_fail.R
@@ -7,6 +7,7 @@ theargs = commandArgs(trailingOnly=TRUE)
 package=theargs[1]
 version=theargs[2]
 
+install.packages('remotes', dependencies=TRUE, repos='https://cran.rstudio.com')
 remotes::install_version(package, version = version, repos = 'https://cran.rstudio.com')
 
 if ( ! library(package, character.only=TRUE, logical.return=TRUE) ) {
